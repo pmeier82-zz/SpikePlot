f26c6701e34678490ff0df0d975dd7896f71bb0d
Philipp Meier <pmeier82@googlemail.com>
2012-03-26 11:46:05 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@
 """install script for the SpikeEval package"""
 __docformat__ = 'restructuredtext'
 
-from setuptools import setup
+from distutils.core import setup
 
 def find_version():
     """read version from __init__"""
@@ -29,34 +29,30 @@ def find_version():
 
 DESC_TITLE = "SpikePlot : plotting package for spike sorting applications"
 DESC_LONG = ''.join([DESC_TITLE, '\n\n', open('README', 'r').read()])
-VERSION = find_version()
 
 if __name__ == "__main__":
-    setup(name="SpikePlot",
-          version=VERSION,
-          packages=['spikeplot'],
-          include_package_data=True,
-          install_requires=[
-              'scipy>=0.6.0',
-              'matplotlib',
-              ],
-          requires=[],
+    setup(
+        #main
+        name="SpikePlot",
+        version=find_version(),
+        packages=['spikeplot'],
+        requires=['scipy (>=0.6.0)', 'matplotlib'],
 
-          # metadata
-          author="Philipp Meier",
-          author_email="pmeier82@googlemail.com",
-          maintainer="Philipp Meier",
-          maintainer_email="pmeier82@googlemail.com",
-          description=DESC_TITLE,
-          long_description=DESC_LONG,
-          license="MIT License",
-          url='http://ni.tu-berlin.de',
-          classifiers=[
-              'Development Status :: 4 - Beta',
-              'Intended Audience :: Science/Research',
-              'License :: OSI Approved :: MIT License',
-              'Natural Language :: English',
-              'Operating System :: OS Independent',
-              'Programming Language :: Python',
-              'Topic :: Scientific/Engineering :: Bio-Informatics'
-          ])
+        # metadata
+        author="Philipp Meier",
+        author_email="pmeier82@googlemail.com",
+        maintainer="Philipp Meier",
+        maintainer_email="pmeier82@googlemail.com",
+        description=DESC_TITLE,
+        long_description=DESC_LONG,
+        license="MIT License",
+        url='http://ni.tu-berlin.de',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Scientific/Engineering :: Bio-Informatics'
+        ])
